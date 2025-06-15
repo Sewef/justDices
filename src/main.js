64f19cde9435511be9dedc1d52e54a2a459e741d
Sewef <elagroum@gmail.com>
2025-06-15 17:12:15 +0200

    quick roll 2

--- src/main.js
+++ src/main.js
@@ -4,15 +4,18 @@ import javascriptLogo from './javascript.svg'
 import { setupDiceRoller } from './roller.js';
 
 document.querySelector('#app').innerHTML = `
-  <form id="input">
-    <div class="input-group">
-      <input type="text" id="inputField" placeholder="1d8+6..." />
-      <button type="button" id="hiddenRollButton" title="Hidden roll">🙈</button>
-      <button type="button" id="rollButton" title="Roll it">⚔️</button>
-      <button type="submit" style="display: none;"></button>
-    </div>
-  </form>
-  <button id="toggleDicePanel" title="Quick Rolls panel">🎲</button>
+  <div id="inputRow">
+    <form id="input">
+      <div class="input-group">
+        <input type="text" id="inputField" placeholder="1d8+6..." />
+        <button type="button" id="hiddenRollButton" title="Hidden roll">🙈</button>
+        <button type="button" id="rollButton" title="Roll it">⚔️</button>
+        <button type="submit" style="display: none;"></button>
+      </div>
+    </form>
+    <button id="toggleDicePanel" title="Quick Rolls panel">🎲</button>
+  </div>
+
   <div id="logContainer">
     <h3>Rolls History</h3>
     <div id="logCards">
